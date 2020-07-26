111b7946c3eea035a4cf9337adc31f3dc9f29490
lizx <1181580150@qq.com>
2020-07-26 18:56:44 +0800

    code review

--- src/main/java/com/example/h2/alibaba/code/review/InterfaceTest.java
+++ src/main/java/com/example/h2/alibaba/code/review/InterfaceTest.java
@@ -5,7 +5,7 @@ package com.example.h2.alibaba.code.review;
  * @date 2020/07/26
  * @Desscrption 测试接口
  **/
-public interface DemoTest01 {
+public interface InterfaceTest {
     /**
      * 修饰符必须是default
      */
@@ -13,9 +13,9 @@ public interface DemoTest01 {
         System.out.println("jdk1.8之后，接口允许有默认方法,但是这个默认方法必须是对所有实现类都有价值");
     };
 }
-class Test01 implements  DemoTest01{
+class Test01 implements  InterfaceTest{
     public static void main(String[] args) {
-        DemoTest01 demoTest01 = new Test01();
+        InterfaceTest demoTest01 = new Test01();
         demoTest01.test();
     }
 }
