875c5d571e1a437fe91fbebe6498bf48b6b81605
lizx <1181580150@qq.com>
2020-09-09 14:24:23 +0800

    update

---
+++ jdk/src/main/java/com/zx/arch/resource/LoadResourceByClass.java
@@ -0,0 +1,37 @@
+package com.zx.arch.resource;
+
+import com.zx.arch.review.EnumTest;
+
+import java.net.URISyntaxException;
+
+/**
+ * @author lizx
+ * @since 1.0.0
+ * @description 通过当前类加载项目资源
+ * @note 使用class加载path 路径最好以"/"开头,这样会到类路径的根路径下去找,通常情况下我们会把资源放在resource文件下 ！resource经过编译会跑到根路径下
+ *        如果不以"/"开头，会默认到所用的class所在包下面找,基本找不到。
+ **/
+public class LoadResourceByClass {
+    private static final String IMAGE_PATH_FORMAT = "/static/%s.png";
+    private static final String IMAGE_NAME = "android";
+    private static String path;
+    static {
+        path = String.format(IMAGE_PATH_FORMAT, IMAGE_NAME);
+    }
+
+    private static void getImage() throws URISyntaxException {
+
+        System.out.println(LoadResourceByClass.class.getResource(path).getPath());
+        System.out.println(EnumTest.class.getResource("").getPath());
+        System.out.println(LoadResourceByClass.class.getResource("/").getPath());
+
+        ///D:/JetBrains/workspace/h2/jdk/target/classes/static/android.png
+        ///D:/JetBrains/workspace/h2/jdk/target/classes/com/zx/arch/resource/
+        ///D:/JetBrains/workspace/h2/jdk/target/classes/
+    }
+
+    public static void main(String[] args) throws URISyntaxException {
+        getImage();
+        //综上所述，要么class加载资源,要么path首字母必须加上 "/"
+    }
+}
--- jdk/src/main/java/com/zx/arch/resource/LoadResourceByClassLoader.java
+++ jdk/src/main/java/com/zx/arch/resource/LoadResourceByClassLoader.java
@@ -12,11 +12,11 @@ import java.net.URL;
 /**
  * @author lizx
  * @since 1.0.0
- * @description 加载项目资源
+ * @description 通过类加载器加载项目资源
  * @note 资源放入static包之后要编译下,ClassPathResource加载的是类路径下的资源
  **/
 @Log
-public class LoadResource {
+public class LoadResourceByClassLoader {
     private static final String IMAGE_PATH_FORMAT = "static/%s.png";
     private static final String IMAGE_NAME = "android";
     private static String path;
@@ -24,21 +24,15 @@ public class LoadResource {
         path = String.format(IMAGE_PATH_FORMAT, IMAGE_NAME);
     }
 
-    /**
-     * 获取图片资源
-     * @throws IOException
-     */
     private static void getImage() throws IOException, URISyntaxException {
         /**
-         * Spring的ClassPathResource类
+         * 1、Spring的ClassPathResource类
          */
-
         Resource imageRes = new ClassPathResource(path);
         log.info(String.valueOf(imageRes.getURI()));
 
-
         /**
-         * 纯jdk
+         * 2、纯jdk
          * ClassLoader  通过ClassUtils获得应用加载类 即AppClassLoader
          */
         ClassLoader classLoader = ClassUtils.getDefaultClassLoader();
@@ -48,5 +42,6 @@ public class LoadResource {
 
     public static void main(String[] args) throws IOException, URISyntaxException {
         getImage();
+        //file:/D:/JetBrains/workspace/h2/jdk/target/classes/static/android.png
     }
 }
