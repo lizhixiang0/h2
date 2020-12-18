786418944a661fed607f61773d78f0e4e84d4455
lizx <1181580150@qq.com>
2020-12-18 14:21:31 +0800

    update

--- mybatis3.3/mybatis-test/src/test/java/com/mybatis/lizx/TestFactory.java
+++ mybatis3.3/mybatis-test/src/test/java/com/mybatis/lizx/TestFactory.java
@@ -9,6 +9,9 @@ import org.junit.Test;
 
 public class TestFactory {
     /*https://blog.csdn.net/qq_28807077/article/details/111322573*/
+    /*https://www.iteye.com/blog/elim-2353672*/
+    /*https://blog.csdn.net/iteye_11305/article/details/82678034?utm_medium=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-2.control&depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-2.control*/
+
     @Test
     public void test(){
         SqlSessionFactory sqlSessionFactory = SqlSessionFactoryUtil.getSqlSessionFactory();
