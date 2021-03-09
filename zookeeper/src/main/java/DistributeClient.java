10f2cda30da24202cd3f2817fa1910a2e859a557
lizx <1181580150@qq.com>
2021-03-09 16:39:07 +0800

    sss

--- zookeeper/src/main/java/DistributeClient.java
+++ zookeeper/src/main/java/DistributeClient.java
@@ -1,7 +1,4 @@
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,9 +8,9 @@ import java.util.List;
  * @since 1.0.0
  **/
 public class DistributeClient {
-    private ZooKeeper zkClient = null;
+    private final static String PARENT_NODE = "/servers";
 
-    private String parentNode = "/servers";
+    private ZooKeeper zkClient = null;
 
     /**
      * 创建到 zkClient 的客户端连接
@@ -21,15 +18,12 @@ public class DistributeClient {
     public void getConnect() throws IOException {
         String connectString = "127.0.0.1:2182,127.0.0.1:2183,127.0.0.1:2184";
         int sessionTimeout = 2000;
-        zkClient = new ZooKeeper(connectString, sessionTimeout, new Watcher() {
-            @Override
-            public void process(WatchedEvent event) {
-                // 再次启动监听
-                try {
-                    getServerList();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+        zkClient = new ZooKeeper(connectString, sessionTimeout, event -> {
+            // 再次启动监听
+            try {
+                getServerList();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         });
     }
@@ -39,27 +33,29 @@ public class DistributeClient {
      */
     public void getServerList() throws Exception {
         // 1 获取服务器子节点信息，并且对父节点进行监听
-        List<String> children = zkClient.getChildren(parentNode, true);
+        List<String> children = zkClient.getChildren(PARENT_NODE, true);
         // 2 存储服务器信息列表
         ArrayList<String> servers = new ArrayList<>();
         // 3 遍历所有节点，获取节点中的主机名称信息
         for (String child : children) {
-            byte[] data = zkClient.getData(parentNode + "/" + child, false, null);
+            byte[] data = zkClient.getData(PARENT_NODE + "/" + child, false, null);
             servers.add(new String(data));
         }
         // 4 打印服务器列表信息
         System.out.println(servers);
     }
 
-    // 业务功能
+    /**
+     * 业务功能
+     */
     public void business() throws Exception {
         System.out.println("client is working ...");
         Thread.sleep(Long.MAX_VALUE);
     }
 
     public static void main(String[] args) throws Exception {
-        // 1 获取 zk 连接
         DistributeClient client = new DistributeClient();
+        // 1 获取 zk 连接
         client.getConnect();
         // 2 获取 servers 的子节点信息，从中获取服务器信息列表
         client.getServerList();
--- zookeeper/src/main/java/DistributeServer.java
+++ zookeeper/src/main/java/DistributeServer.java
@@ -7,6 +7,9 @@ import java.io.IOException;
  * @since 1.0.0
  **/
 public class DistributeServer {
+
+    private final static String PARENT_NODE = "/servers";
+
     private ZooKeeper zkClient = null;
 
     /**
@@ -15,20 +18,14 @@ public class DistributeServer {
     public void getConnect() throws IOException {
         String connectString = "127.0.0.1:2182,127.0.0.1:2183,127.0.0.1:2184";
         int sessionTimeout = 2000;
-        zkClient = new ZooKeeper(connectString, sessionTimeout, new Watcher() {
-            @Override
-            public void process(WatchedEvent event) {
-            }
-        });
+        zkClient = new ZooKeeper(connectString, sessionTimeout, event -> {});
     }
 
     /**
      * 注册服务器
      */
-    public void registServer(String hostname) throws Exception {
-        String parentNode = "/servers";
-        String create = zkClient.create(parentNode + "/server", hostname.getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE,
-                CreateMode.EPHEMERAL_SEQUENTIAL);
+    public void registerServer(String hostname) throws Exception {
+        String create = zkClient.create(PARENT_NODE + "/server", hostname.getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE,CreateMode.EPHEMERAL_SEQUENTIAL);
         System.out.println(hostname + " is online " + create);
     }
 
@@ -41,11 +38,11 @@ public class DistributeServer {
     }
 
     public static void main(String[] args) throws Exception {
-        // 1 获取 zk 连接
         DistributeServer server = new DistributeServer();
+        // 1 获取 zk 连接
         server.getConnect();
         // 2 利用 zk 连接注册服务器信息
-        server.registServer(args[0]);
+        server.registerServer(args[0]);
         // 3 启动业务功能
         server.business(args[0]);
     }
