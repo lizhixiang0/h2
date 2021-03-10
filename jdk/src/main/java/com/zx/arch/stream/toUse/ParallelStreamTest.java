64cccdbae6080e8c3ed8e51f4de185e855d4f636
lizx <1181580150@qq.com>
2021-03-10 13:25:40 +0800

    ss

--- jdk/src/main/java/com/zx/arch/stream/toUse/ParallelStreamTest.java
+++ jdk/src/main/java/com/zx/arch/stream/toUse/ParallelStreamTest.java
@@ -4,7 +4,11 @@ package com.zx.arch.stream.toUse;
  * @author lizx
  * @since 1.0.0
  * @description 介绍并行流以及使用注意事项
+ * @blog stream流的底层原理：https://blog.csdn.net/tyrroo/article/details/81390202
  **/
 public class ParallelStreamTest {
+    public static void main(String[] args) {
+        String s  = "https://blog.csdn.net/qq_38974634/article/details/81347604";
+    }
 
 }
