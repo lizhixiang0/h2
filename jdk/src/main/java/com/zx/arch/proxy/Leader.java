9fe3c61248fb0a520028a2ca933ad7b60aea10a8
lizx <1181580150@qq.com>
2021-03-16 16:34:47 +0800

    ss

--- jdk/src/main/java/com/zx/arch/proxy/Leader.java
+++ jdk/src/main/java/com/zx/arch/proxy/Leader.java
@@ -3,6 +3,10 @@ package com.zx.arch.proxy;
 /**
  * @author lizx
  * @since 1.0.0
+ * @description java的动态代理方式似乎有点多，所以这里做个记录
+ * @blog "https://www.bilibili.com/video/BV1mD4y1d7rt?p=25
+ * @blog "https://blog.csdn.net/lihenair/article/details/69948918
  **/
 public interface Leader {
+
 }
